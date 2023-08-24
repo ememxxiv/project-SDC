1c1fa3865d9723523de9ac05722177b70b31c09a
Manuel Maghari <manuelpmagahriiii@gmail.com>
2023-08-24 18:54:42 +0800

    initial commit

---
+++ resources/views/brgy-home.blade.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>"Name of the Barangay"</title>
+
+
+
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/app.css">
+
+<script src="https://kit.fontawesome.com/0093b2b05a.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+
+<body style="font-family: Open Sans, sans-serif">
+    
+<div class="">
+    <img class="bg" src="./images/green2.jpg" alt="background Picture">
+  </div>    
+    
+  
+  <div>
+
+    <img class="absolute logo rounded-full" src="./images/logo-brgy.jpeg"
+                alt="Logo" width="100" height="100"
+                 class="d-inline-block align-text-center ">
+
+
+
+<nav class="relative py-5 px-5 align-text-center">
+<ul class="nav justify-content-center ">
+
+  <li class="mabuhay nav-item">
+    <a class="nav-link active text-black" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-black text-bold" href="#">Documents</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-black text-bold" href="#">Officials</a>
+  </li>
+    <li class="nav-item">
+    <a class="nav-link text-black text-bold" href="#">Contact us</a>
+  </li>
+    <li class="nav-item">
+    <a class="nav-link text-black text-bold" href="#">About us</a>
+  </li>
+    <li class="nav-item">
+    <a class="nav-link text-black text-bold" href="#">Calendar</a>
+  </li>
+    <li class="nav-item">
+    <a class="nav-link text-black text-bold" href="#">Officials</a>
+  </li>
+   <button href="" class="link-btn ml-3 rounded-full text-xs font-semibold uppercase py-2 px-4">
+    LOGIN
+    </button>
+
+</ul>
+</nav>
+
+
+<div class="header">
+    <header class="relative max-w-xl mx-auto text-center">
+
+            <h1 class="welcome text-5xl">
+                Welcome to<span class="text-white font-bold"> BARANGAY MA-AO </span> </h1>
+            
+              <h5 class="city style: font-size="1rem">Bago City, Negros Occidental</h5>
+
+
+
+                <div class="conlog space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+                    
+                <!--  Category -->
+                    <div class="contact relative flex lg:inline-flex items-center rounded-xl">
+                        <button href="" class=" ml-3 rounded-full text-xs font-semibold  uppercase py-2 px-2">
+                        Contact us
+                        </button>
+                    </div>
+
+                    <!-- Other Filters -->
+                    <div class="relative flex lg:inline-flex items-center rounded-xl">
+                        <button href=""  class="sign ml-3 rounded-full text-xs font-semibold uppercase py-2 px-2">
+                        Sign up now
+                        </button>
+                    </div>
+            </div>
+    </header>
+</div>
+
+<div class="box section-area content-inner service-info-bx">
+                <div class="container">
+					<div class="row">
+						<div class="zoom col-lg-4 col-md-4 col-sm-12">
+							<div class="service-bx">
+								<div class="action-box">
+									<img src="assets/images/our-services/pic1.jpg" alt="">
+								</div>
+								<div class="info-bx text-center px-5">
+									<div class="feature-box-sm radius bg-white py-3 rounded border-b-2">
+										<i class="relative fas fa-landmark px-2 py-2 bg-green-500 rounded-full shadow-slate-100"  style="color: #fafafa; "></i>
+									</div>
+									<h6 class="py-2">Barangay Officials</h6>
+									 <button
+                          href="" 
+                          class="learn ml-3 rounded-full text-xs text-white uppercase py-2 px-2" 
+                          >
+                        Learn more
+                    </button>
+								</div>
+							</div>
+						</div>
+						<div class="zoom col-lg-4 col-md-4 col-sm-6">
+							<div class="service-bx">
+								<div class="action-box">
+									<img src="assets/images/our-services/pic2.jpg" alt="">
+								</div>
+								<div class="info-bx text-center px-5">
+									<div class="feature-box-sm radius bg-white py-3 rounded border-b-2">
+										<i class="fas fa-envelope-open-text px-2 py-2 bg-green-500 rounded-full" style="color: #f9fafb;"></i>
+									</div>
+									<h6 class="py-2">Announcements</h6>
+									<button
+                          href="" 
+                          class="learn ml-3 rounded-full text-xs text-white uppercase py-2 px-2" 
+                          >
+                        Learn more
+                    </button>
+								</div>
+							</div>
+						</div>
+						<div class="zoom col-lg-4 col-md-4 col-sm-6">
+							<div class="service-bx m-b0">
+								<div class="action-box">
+									<img src="assets/images/our-services/pic3.jpg" alt="">
+								</div>
+								<div class="info-bx text-center px-5">
+									<div class="feature-box-sm radius bg-white py-3 rounded border-b-2">
+										<i class="fas fa-book px-2 py-2 bg-green-500 rounded-full" style="color: #fcfcfc;"></i>
+									</div>
+									<h6 class="py-2">Issuance</h6>
+									<button
+                          href="" 
+                          class="learn ml-3 rounded-full text-xs text-white uppercase py-2 px-2" 
+                          >
+                        Learn more
+                    </button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+
+
+<footer class="bg-grey border-opacity-5 rounded-xl text-center px-10 mt-16">
+              
+<p class="text-sm mt-3">Empowered by SDC</p>
+</footer>
+
+
+
+
+</body>
+
+</html>
+
+{{-- 
+        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+            <img src="./images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
+            <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
+
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+
+                    <form method="POST" action="#" class="lg:flex text-sm">
+                        <div class="lg:py-3 lg:px-5 flex items-center">
+                            <label for="email" class="hidden lg:inline-block">
+                                <img src="./images/mailbox-icon.svg" alt="mailbox letter">
+                            </label>
+
+                            <input id="email" type="text" placeholder="Your email address"
+                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                        </div>
+
+                        <button type="submit"
+                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
+                        >
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </section> --}}
